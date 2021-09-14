e43bf43e7f956d948e4d4e70cb9cd37465630936
DESKTOP-BSPI59J\bebeb <mangnangrang>
2021-09-14 15:28:51 +0900

    [기초-데이터형]

---
+++ codeup1029.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class codeup1029 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        Double a = sc.nextDouble();
+        System.out.printf("%.11f",a);
+    }
+    
+}
