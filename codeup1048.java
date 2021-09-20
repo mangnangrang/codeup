b759488dde4b4c94ac7b848354e531d23cff797b
DESKTOP-BSPI59J\bebeb <mangnangrang>
2021-09-20 15:11:09 +0900

    [기초-비트시프트연산]

---
+++ codeup1048.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class codeup1048 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a<<b;
+        System.out.println(c);
+    }
+
+}
